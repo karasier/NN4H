84c247cea312e49eb2a67dbf30c60f4bc6ddba70
GMNsh <52313638+GMNsh@users.noreply.github.com>
2020-08-07 19:29:31 +0900

    Update lstm.rb
    coding

--- LSTM/lstm.rb
+++ LSTM/lstm.rb
@@ -1,1 +1,18 @@
 repuire_realative 'FastNeurons-1.2.0/lib/fast_neurons'
+
+
+nn_foget = FastNeurons::NN.new([2,1],[:Sigmoid])
+
+nn_input0 = FastNeurons::NN.new([2,1],[:Sigmoid])
+
+nn_input1 = FastNeurons::NN.new([2,1],[:Tanh])
+
+nn_output0 = FastNeurons::NN.new([2,1],[:Sigmoid])
+
+nn_output1 = FastNeurons::NN.new([2,1],[:Tanh])
+
+nn_foget.randomize
+nn_input0.randomize
+nn_input1.randomize
+nn_output0.randomize
+nn_output1.randomize
