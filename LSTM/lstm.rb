146f25c603839cb1db0d3ccfac7b25b4ba09134d
GMNsh <52313638+GMNsh@users.noreply.github.com>
2020-10-29 20:37:16 +0900

    LSTMCell接続の実装

--- LSTM/lstm.rb
+++ LSTM/lstm.rb
@@ -18,24 +18,37 @@ class LSTMCell
   end
 
   def input(x, c, h)
-    #@x_in = N[x, dtype: :float]
-    #@c_in = N[c, dtype: :float]
-    #@h_in = N[h, dtype: :float]
+    # @x_in = N[x, dtype: :float]
+    # @c_in = N[c, dtype: :float]
+    # @h_in = N[h, dtype: :float]
+
+    # @x_in = N[x.clone.flatten, dtype: :float64].transpose
+    # @c_in = N[c.clone.flatten, dtype: :float64].transpose
+    # @h_in = N[h.clone.flatten, dtype: :float64].transpose
+
     @x_in = x.clone
     @c_in = c.clone
     @h_in = h.clone
 
+     puts "\ninput: data"
+     print @x_in
+     puts
+     print @c_in
+     puts
+     print @h_in
+     puts
   end
 
   def output
-    return [@h_out.to_a, @c_out.to_a]
+    #return [@c_out.to_a, @h_out.to_a]
+    return [@c_out, @h_out]
   end
 
   def propagate
 
-
     #忘却ゲート
     uvw = proc { N[@c_in, dtype: :float64].transpose * @nn_forget.get_outputs }
+    # uvw = proc { @c_in * @nn_forget.get_outputs }
 
     #入力ゲート
     xyz = proc { @nn_input0.get_outputs * @nn_input1.get_outputs }
@@ -48,7 +61,18 @@ class LSTMCell
 
     nn_output1 = proc { pqr.call * @nn_output0.get_outputs}
 
+    # puts "\npropagate:@x_in,@c_in"
+    # print @x_in
+    # puts
+    # print @c_in
+    # puts
+
+    puts "class: @x_in, @c_in"
+    puts @x_in.class
+    puts @c_in.class
+
     nn_inputs = @x_in + @c_in
+    # print nn_inputs
     @nn_forget.input(nn_inputs)
     @nn_input0.input(nn_inputs)
     @nn_input1.input(nn_inputs)
@@ -61,6 +85,7 @@ class LSTMCell
 
     @h_out = nn_output1.call
     @c_out = abc.call
+
   end
 
   def back_propagate
@@ -75,16 +100,44 @@ end
 class LSTM
 
   def initialize(cells_num, x_size, h_size)
-      @cells = cells_num.times { LSTMCell.new(x_size, h_size) }
+    @cells = cells_num.times.map { LSTMCell.new(x_size, h_size) }
   end
 
   def input(x)
-     @xs = x.to_a
-
+    @xs = x.to_a
+    #puts @cells[0]
+    # @cells.each do |i|
+    #   @cells[i].input(@xs[i],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    #                  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    # end
+    @cells[0].input(@xs[0],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
   end
 
   def propagate
+    puts "\ncells[0]: propagate"
+    @cells[0].propagate
+    @c,@h = @cells[0].output
+    # puts "output data"
+    # puts @c
+    # puts @h
 
+    @cells[1].input(@xs[1],@c,@h)
+    puts "\ncells[1]: propagate"
+    @cells[1].propagate
+    # puts "output data"
+    # puts @c
+    # puts @h
 
+    # puts @cells.length
+    #  for i in 1..@cells.length-1 do
+    #    print @cells[i-1].output
+    #    puts
+    #    @c_in, @h_in =@cells[i-1].output
+    #    print @c_in
+    #    puts
+    #    print @h_in
+    #    @cells[i].input(@xs[i], @c_in, @h_in)
+    #    @cells[i].propagate
+    #  end
   end
 end
--- LSTM/lstm_test.rb
+++ LSTM/lstm_test.rb
@@ -2,17 +2,25 @@ require_relative '..\..\FastNeurons\lib\fast_neurons.rb'
 require_relative './lstm.rb'
 
 #LSTMCell test
-cell = LSTMCell.new(7,7)
 
-cell.input([1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0],
-           [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-           [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+# cell = LSTMCell.new(7,7)
 
-cell.propagate
+# cell.input([1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0],
+#            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+#            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-print cell.output
+# cell.propagate
+# c,h = cell.output
+# puts c
+# puts h
 
 #LSTM test
 
 #lstm = LSTM.new(1000,7,7)
-lstm = LSTM.new(2,7,7)
+x = [[1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0],
+     [0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0],
+     [1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0]]
+lstm = LSTM.new(3,7,7)
+lstm.input(x)
+lstm.propagate
+#print lstm.output
