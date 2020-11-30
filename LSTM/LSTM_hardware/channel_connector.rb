6d89b2eacbf15e0be03af9bd8a3a57b921487998
GMNsh <52313638+GMNsh@users.noreply.github.com>
2020-11-30 21:20:17 +0900

    connectorのテスト

---
+++ LSTM/LSTM_hardware/channel_connector.rb
@@ -0,0 +1,93 @@
+require 'std/memory.rb'
+require 'std/linear.rb'
+require "std/fixpoint.rb"
+require_relative "network_constructor.rb"
+require_relative "network_loader.rb"
+require_relative "quantize.rb"
+require 'std/channel.rb'
+require 'std/connector.rb'
+
+include HDLRuby::High::Std
+
+system :channel_connector do
+    # データ型の宣言
+    integer_width = 4 # 整数部のビット幅
+    decimal_width = 4 # 実数部のビット幅
+    address_width = 4 # lutのアドレスのビット幅
+    typ = signed[integer_width, decimal_width] # データ型
+
+    inputs = [1, 1]
+    columns = [2, 2, 1]
+
+    inner :clk,   # clock
+          :rst,   # reset
+          :req,   # request
+          :fill   # 入力値のメモリへの書き込み
+
+    # inner :ack_fill_sig, # 書き込みのack:tanh
+          # :ack_network_sig, # ニューラルネット:ワークのack:tanh
+
+
+    mem_rom(typ, columns[0], clk, rst, inputs, rinc: :rst, winc: :rst).(:rom_inputs_x) # 入力値を格納するrom(x)
+
+    mem_rom(typ, columns[0], clk, rst, inputs, rinc: :rst, winc: :rst).(:rom_inputs_h) # 入力値を格納するrom(h)
+
+    mem_file(typ, columns[0], clk, rst, rinc: :rst, winc: :rst).(:ram_inputs) # 出力値を格納するram(sig)
+
+    reader_inputs_x = rom_inputs_x.branch(:rinc) #入力値xの読みだし用branch
+    reader_inputs_h = rom_inputs_h.branch(:rinc) #入力値hの読みだし用branch
+    reader_inputs = ram_inputs.branch(:winc) #入力値を合成した値の書き込み用branch
+
+    merger([typ]*2,clk.negedge,[reader_inputs_x,reader_inputs_h],reader_inputs)
+
+    timed do
+      # リセット
+      clk <= 0
+      rst <= 0
+      req <= 0
+      fill <= 0
+      !10.ps
+
+      # メモリ読み出し位置の初期化
+      rst <= 1
+      !10.ps
+      clk <= 1
+      !10.ps
+      clk <= 0
+      !10.ps
+      clk <= 1
+      !10.ps
+
+      # パラメータのメモリへの書き込み
+      clk <= 0
+      rst <= 0
+      fill <= 1
+
+      !10.ps
+      10.times do |i|
+        clk <= 1
+        !10.ps
+        clk <= 0
+        !10.ps
+      end
+
+      fill <= 0
+      clk <= 1
+      !10.ps
+
+      # 計算の実行
+      clk <= 0
+      req <= 1
+      !10.ps
+      clk <= 1
+      !10.ps
+      clk <= 0
+      !10.ps
+      30.times do
+        clk <= 1
+        !10.ps
+        clk <= 0
+        !10.ps
+      end
+    end
+end
