25af3ac8514b520c832997150ce86cf0d6dc77a2
karasi <40154200+karasier@users.noreply.github.com>
2020-12-02 21:31:15 +0900

    Added an example of channel.
    ・channelの使用例を追加した。

---
+++ examples/ch_example.rb
@@ -0,0 +1,26 @@
+system :ch_example do
+  channel0 :ch0
+  channel1([8], 256).(:ch1)
+
+  p0 = ch1.branch(:port0)
+  addr0 = port0.wrap(0)
+end
+
+system :ch_access do |channel|
+  input :clk, :rst, :ch_read, :ch_write
+
+  [8].inner :counter
+  [8].inner :value
+
+  par(clk.posedge) do
+    hif(rst) do
+      counter <= 0
+    end 
+    helsif(ch_write) do
+      channel.write(counter) { counter <= counter + 1 }
+    end
+    helsif(ch_read) do
+      channel.read(value)
+    end
+  end
+end
